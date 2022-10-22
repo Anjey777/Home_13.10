3d2291422b4d6f54ea796f9fa8c5654cfdc23510
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-10-22 12:39:41 +0300

    68 finish

---
+++ 64/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int n = InputInt("Введите число N  ");
+int m = 1;
+Console.WriteLine($"n={n}, m={m}");
+
+Console.WriteLine(recursion(n, m));
+
+int recursion(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{recursion(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
---
+++ 68/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+все делать через рекурсию
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите два числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akker(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+int Akker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Akker(m - 1, 1);
+    }
+    return Akker(m - 1, Akker(m, n - 1));
+}
+
+Console.WriteLine(Akker(m, n));
